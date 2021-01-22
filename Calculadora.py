a717b02bfa85763121e4bc7c4b249d079409f11c
Jorge Arguello <jorge.arguello1999@gmail.com>
2021-01-22 02:37:26 -0500

    Se acaba la pequeña implementacion Ahora no es una calculadora quimica Ahora es una calculadora con funciones raras o divertidas para entretenerse un rato

--- Calculadora.py
+++ Calculadora.py
@@ -1,109 +1,251 @@
 ###Caligrafias
-Calculadora_Temperatura = ("""  _____      _            _           _                       _      
- / ____|    | |          | |         | |                     | |     
-| |     __ _| | ___ _   _| | __ _  __| | ___  _ __ __ _    __| | ___ 
-| |    / _` | |/ __| | | | |/ _` |/ _` |/ _ \| '__/ _` |  / _` |/ _ \
-| |___| (_| | | (__| |_| | | (_| | (_| | (_) | | | (_| | | (_| |  __/
- \_____\__,_|_|\___|\__,_|_|\__,_|\__,_|\___/|_|  \__,_|  \__,_|\___|
-                                                                     
-                                                                     
- _______                                  _                   
-|__   __|                                | |                  
-   | | ___ _ __ ___  _ __   ___ _ __ __ _| |_ _   _ _ __ __ _ 
-   | |/ _ \ '_ ` _ \| '_ \ / _ \ '__/ _` | __| | | | '__/ _` |
-   | |  __/ | | | | | |_) |  __/ | | (_| | |_| |_| | | | (_| |
-   |_|\___|_| |_| |_| .__/ \___|_|  \__,_|\__|\__,_|_|  \__,_|
-                    | |                                       
-                    |_|                                     """)
+Calculadora_Temperatura = ("""
+ ######     ###    ##        ######  ##     ## ##          ###    ########   #######  ########     ###
+##    ##   ## ##   ##       ##    ## ##     ## ##         ## ##   ##     ## ##     ## ##     ##   ## ##
+##        ##   ##  ##       ##       ##     ## ##        ##   ##  ##     ## ##     ## ##     ##  ##   ##
+##       ##     ## ##       ##       ##     ## ##       ##     ## ##     ## ##     ## ########  ##     ##
+##       ######### ##       ##       ##     ## ##       ######### ##     ## ##     ## ##   ##   #########
+##    ## ##     ## ##       ##    ## ##     ## ##       ##     ## ##     ## ##     ## ##    ##  ##     ##
+ ######  ##     ## ########  ######   #######  ######## ##     ## ########   #######  ##     ## ##     ##
 
-
-
-
+#######
+   #    ###### #    # #####  ###### #####    ##   ##### #    # #####    ##
+   #    #      ##  ## #    # #      #    #  #  #    #   #    # #    #  #  #
+   #    #####  # ## # #    # #####  #    # #    #   #   #    # #    # #    #
+   #    #      #    # #####  #      #####  ######   #   #    # #####  ######
+   #    #      #    # #      #      #   #  #    #   #   #    # #   #  #    #
+   #    ###### #    # #      ###### #    # #    #   #    ####  #    # #    #
+ """)
 Separador = "#############################################################################################################################################################\n"
+Calculadora_Magica = ("""
+ ######     ###    ##        ######  ##     ## ##          ###    ########   #######  ########     ###
+##    ##   ## ##   ##       ##    ## ##     ## ##         ## ##   ##     ## ##     ## ##     ##   ## ##
+##        ##   ##  ##       ##       ##     ## ##        ##   ##  ##     ## ##     ## ##     ##  ##   ##
+##       ##     ## ##       ##       ##     ## ##       ##     ## ##     ## ##     ## ########  ##     ##
+##       ######### ##       ##       ##     ## ##       ######### ##     ## ##     ## ##   ##   #########
+##    ## ##     ## ##       ##    ## ##     ## ##       ##     ## ##     ## ##     ## ##    ##  ##     ##
+ ######  ##     ## ########  ######   #######  ######## ##     ## ########   #######  ##     ## ##     ##
 
+ ##     ##    ###     ######   ####  ######     ###
+ ###   ###   ## ##   ##    ##   ##  ##    ##   ## ##
+ #### ####  ##   ##  ##         ##  ##        ##   ##
+ ## ### ## ##     ## ##   ####  ##  ##       ##     ##
+ ##     ## ######### ##    ##   ##  ##       #########
+ ##     ## ##     ## ##    ##   ##  ##    ## ##     ##
+ ##     ## ##     ##  ######   ####  ######  ##     ##
+ """)
+Calculadora_Calorias = ("""
+ ######     ###    ##        ######  ##     ## ##          ###    ########   #######  ########     ###
+##    ##   ## ##   ##       ##    ## ##     ## ##         ## ##   ##     ## ##     ## ##     ##   ## ##
+##        ##   ##  ##       ##       ##     ## ##        ##   ##  ##     ## ##     ## ##     ##  ##   ##
+##       ##     ## ##       ##       ##     ## ##       ##     ## ##     ## ##     ## ########  ##     ##
+##       ######### ##       ##       ##     ## ##       ######### ##     ## ##     ## ##   ##   #########
+##    ## ##     ## ##       ##    ## ##     ## ##       ##     ## ##     ## ##     ## ##    ##  ##     ##
+ ######  ##     ## ########  ######   #######  ######## ##     ## ########   #######  ##     ## ##     ##
 
-print("""
-    
-  /$$$$$$            /$$                     /$$                 /$$                             
- /$$__  $$          | $$                    | $$                | $$                             
-| $$  \__/  /$$$$$$ | $$  /$$$$$$$ /$$   /$$| $$  /$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$ 
-| $$       |____  $$| $$ /$$_____/| $$  | $$| $$ |____  $$ /$$__  $$ /$$__  $$ /$$__  $$|____  $$
-| $$        /$$$$$$$| $$| $$      | $$  | $$| $$  /$$$$$$$| $$  | $$| $$  \ $$| $$  \__/ /$$$$$$$
-| $$    $$ /$$__  $$| $$| $$      | $$  | $$| $$ /$$__  $$| $$  | $$| $$  | $$| $$      /$$__  $$
-|  $$$$$$/|  $$$$$$$| $$|  $$$$$$$|  $$$$$$/| $$|  $$$$$$$|  $$$$$$$|  $$$$$$/| $$     |  $$$$$$$
- \______/  \_______/|__/ \_______/ \______/ |__/ \_______/ \_______/ \______/ |__/      \_______/
-                                                                                                 
-                                                                                                 
-                                                                                                 
-  /$$$$$$                          /$$                                                           
- /$$__  $$                        |__/                                                           
-| $$  \ $$ /$$   /$$ /$$$$$$/$$$$  /$$  /$$$$$$$  /$$$$$$                                        
-| $$  | $$| $$  | $$| $$_  $$_  $$| $$ /$$_____/ |____  $$                                       
-| $$  | $$| $$  | $$| $$ \ $$ \ $$| $$| $$        /$$$$$$$                                       
-| $$/$$ $$| $$  | $$| $$ | $$ | $$| $$| $$       /$$__  $$                                       
-|  $$$$$$/|  $$$$$$/| $$ | $$ | $$| $$|  $$$$$$$|  $$$$$$$                                       
- \____ $$$ \______/ |__/ |__/ |__/|__/ \_______/ \_______/                                       
-      \__/                                                                                       
-                                                                                                 
+ #####     #    #       ####### ######  ###    #     #####
+#     #   # #   #       #     # #     #  #    # #   #     #
+#        #   #  #       #     # #     #  #   #   #  #
+#       #     # #       #     # ######   #  #     #  #####
+#       ####### #       #     # #   #    #  #######       #
+#     # #     # #       #     # #    #   #  #     # #     #
+ #####  #     # ####### ####### #     # ### #     #  #####
+""")
+Calculadora_Porcentaje = ("""
+ ######     ###    ##        ######  ##     ## ##          ###    ########   #######  ########     ###
+##    ##   ## ##   ##       ##    ## ##     ## ##         ## ##   ##     ## ##     ## ##     ##   ## ##
+##        ##   ##  ##       ##       ##     ## ##        ##   ##  ##     ## ##     ## ##     ##  ##   ##
+##       ##     ## ##       ##       ##     ## ##       ##     ## ##     ## ##     ## ########  ##     ##
+##       ######### ##       ##       ##     ## ##       ######### ##     ## ##     ## ##   ##   #########
+##    ## ##     ## ##       ##    ## ##     ## ##       ##     ## ##     ## ##     ## ##    ##  ##     ##
+ ######  ##     ## ########  ######   #######  ######## ##     ## ########   #######  ##     ## ##     ##
+
+######  ####### ######   #####  ####### #     # #######    #          # #######
+#     # #     # #     # #     # #       ##    #    #      # #         # #
+#     # #     # #     # #       #       # #   #    #     #   #        # #
+######  #     # ######  #       #####   #  #  #    #    #     #       # #####
+#       #     # #   #   #       #       #   # #    #    ####### #     # #
+#       #     # #    #  #     # #       #    ##    #    #     # #     # #
+#       ####### #     #  #####  ####### #     #    #    #     #  #####  #######
+
+""")
+PesoPlaneta = ("""
+####### #     #    ######  #######  #####  #######
+   #    #     #    #     # #       #     # #     #
+   #    #     #    #     # #       #       #     #
+   #    #     #    ######  #####    #####  #     #
+   #    #     #    #       #             # #     #
+   #    #     #    #       #       #     # #     #
+   #     #####     #       #######  #####  #######
+
+####### #     #    ####### ####### ######  #######
+#       ##    #    #     #    #    #     # #     #
+#       # #   #    #     #    #    #     # #     #
+#####   #  #  #    #     #    #    ######  #     #
+#       #   # #    #     #    #    #   #   #     #
+#       #    ##    #     #    #    #    #  #     #
+####### #     #    #######    #    #     # #######
+
+######  #          #    #     # ####### #######    #
+#     # #         # #   ##    # #          #      # #
+#     # #        #   #  # #   # #          #     #   #
+######  #       #     # #  #  # #####      #    #     #
+#       #       ####### #   # # #          #    #######
+#       #       #     # #    ## #          #    #     #
+#       ####### #     # #     # #######    #    #     #
 """)
 
 ###Definir que quiere hacer el usuario
-    
-opciones = float(input('Calculadora de Temperatura =     ("0")\nCalculadora de Longitud    =     ("1")\nCual eliges________________=     '))
-
-print(Separador)
+print(Calculadora_Magica)
+opciones = float(input('Calculadora de Temperatura =     ("0")\nCalculadora de Calorias    =     ("1")\nCalcula el Porcentaje      =     ("2")\nTu peso en otro Planeta    =     ("3")\nCual eliges________________=     '))
+print("\n",Separador)
 
 ###Calculadora de Temperatura
-###Ya acabe con las conversiones de temperatura 
-while opciones <= 2:
+###Ya acabe con las conversiones de temperatura
+if opciones == 0:
 
-    if opciones == 0:
-       
-        print (Calculadora_Temperatura)
-        opcionesTemperatura = int(input('Convertir: ºF a ºC___________("0")\nConvertir: ºC a ºF___________("1")\nConvertir: ºF a ºK___________("2")\nConvertir: ºK a ºF___________("3")\nConvertir: '))
-        print (Separador)
+    print (Calculadora_Temperatura)
+    opcionesTemperatura = int(input('Convertir: ºF a ºC___________("0")\nConvertir: ºC a ºF___________("1")\nConvertir: ºF a ºK___________("2")\nConvertir: ºK a ºF___________("3")\nConvertir: '))
+    print ("\n",Separador)
 
-        while opcionesTemperatura <= 3:
-            if opcionesTemperatura == 0:
 
-                F= float(input("Fahrenheit_a_Celsius..."))
-                C= (F-32)/18
-                print (C)
+    if opcionesTemperatura == 0:
 
-                break
+        F= float(input("Fahrenheit_a_Celsius: "))
+        C= (F-32)/18
+        nombre = C
 
-            elif opcionesTemperatura == 1:
-               
-                C = float(input("Celsius_a_Fahrenheit: "))
-                F = (1.8*C) + 32
-                print (F)
-                
-                break
 
-            elif opcionesTemperatura == 2:
+    elif opcionesTemperatura == 1:
 
-                F = float(input("Fahrenheit_a_Kelvin: "))
-                K = (F + 459.67)/1.8
-                print (K)
+        C = float(input("Celsius_a_Fahrenheit: "))
+        F = (1.8*C) + 32
+        nombre = F
 
-                break
 
-            elif opcionesTemperatura == 3:
+    elif opcionesTemperatura == 2:
 
-                K = float(input("Kelvin_a_Fahrenheit: "))
-                F = (9/5)*K - 459.67
-                print(F)
+        F = float(input("Fahrenheit_a_Kelvin: "))
+        K = (F + 459.67)/1.8
+        nombre = K
 
-                break
 
-        break
+    elif opcionesTemperatura == 3:
 
-    elif opciones == 1:
-       
-        print ("Calculadora de Longitud")
-        
-        break
+        K = float(input("Kelvin_a_Fahrenheit: "))
+        F = (9/5)*K - 459.67
+        nombre = F
 
+
+    print("\nLa cantidad es: ", nombre)
+
+
+elif opciones == 1:
+
+    print (Calculadora_Calorias)
+    #Colocar la caligrafia en la parte superior
+
+    Peso = float(input("\nCual es tu peso en KG?        "))
+    Estatura = float(input("Cual es tu altura en CM?      "))
+    Edad = float(input("Cual es tu edad?              "))
+    Genero = float(input('Hombre = "1" y Mujer = "2"    '))
+
+    Valor1 = Peso * 10
+    Valor2 = Estatura * 6.25
+    Valor3 = Edad * 5
+    ValorTotal = Valor1 + Valor2 - Valor3
+
+    Mujer = ValorTotal - 161
+    Hombre = ValorTotal + 5
+
+    Sedentario = Hombre * 1.2
+
+    print("\n",Separador)
+
+    print("Elige la cantidad de ejercicio que realizas a la semana\n")
+    Complemento = float(input('Sedentario                     =       "1"\n1-3 dias de ejercicio          =       "2"\n3-5 dias de ejercicio          =       "3"\n6-7 dias de ejercicio          =       "4"\nAtletas o actividad extrema    =       "5"\n\nEleccion: '))
+
+    print("\n", Separador)
+
+    if Genero == 1:
+        print("En total quemas")
+        if Complemento == 1:
+            print(Hombre * 1.2)
+        elif Complemento == 2:
+            print(Hombre * 1.375)
+        elif Complemento == 3:
+            print(Hombre * 1.55)
+        elif Complemento == 4:
+            print(Hombre * 1.72)
+        elif Complemento == 5:
+            print(Hombre * 1.9)
     else:
-       
-        print ("Chao...")
+        print("En total quemas")
+        if Complemento == 1:
+            print(Mujer * 1.2)
+        elif Complemento == 2:
+            print(Mujer * 1.375)
+        elif Complemento == 3:
+            print(Mujer * 1.55)
+        elif Complemento == 4:
+            print(Mujer * 1.72)
+        elif Complemento == 5:
+            print(Mujer * 1.9)
+
+elif opciones == 2:
+
+    #Añadir caligrafias
+    print (Calculadora_Porcentaje)
+
+    primva = float(input("Ingresa el valor del cual quieres saber su porcentaje =        "))
+    porcen = float(input("Ingresa el porcentaje que quieres saber               =        "))
+    primva = (primva*porcen)/100
+    print("\nEl porcentaje es: ", primva)
+
+
+elif opciones == 3:
+
+    print(PesoPlaneta)
+    print(Separador)
+    tuPeso = float(input("Tu peso en Kg  =   "))
+    tuPlaneta = float(input('\nMarte    =   ("1")\nMercurio =   ("2")\nUrano    =   ("3")\nNeptuno  =   ("4")\nSaturno  =   ("5")\nJupiter  =   ("6")\nEleccion: '))
+    print(Separador)
+
+    g_Marte = 3.71
+    g_Mercurio = 3.70
+    g_Urano = 8.69
+    g_Neptuno = 11
+    g_Saturno = 8.96
+    g_Jupiter = 23.12
+    g_Tierra = 9.8
+
+    if tuPlaneta == 1:
+        Planeta = g_Marte
+        nombre = "Marte"
+
+    elif tuPlaneta == 2:
+        Planeta = g_Mercurio
+        nombre = "Mercurio"
+
+    elif tuPlaneta == 3:
+        Planeta = g_Urano
+        nombre = "Urano"
+
+    elif tuPlaneta == 4:
+        Planeta = g_Neptuno
+        nombre = "Neptuno"
+
+    elif tuPlaneta == 5:
+        Planeta = g_Saturno
+        nombre = "Saturno"
+
+    elif tuPlaneta == 6:
+        Planeta = g_Jupiter
+        nombre = "Jupiter"
+
+    pesoFinal = (tuPeso*Planeta)/g_Tierra
+    print(pesoFinal) 
+
+else:
+
+    print ("Chao...")
