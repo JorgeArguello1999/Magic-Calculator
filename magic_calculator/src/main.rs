d16315b336be59ab8c02dfb59a60936fd28dba87
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-04-09 22:49:48 -0500

    work: functions for weight

--- magic_calculator/src/main.rs
+++ magic_calculator/src/main.rs
@@ -3,6 +3,9 @@ mod weight_another_planets;
 fn main() {
     println!("Hello, world!");
     
-    let calculate = weight_another_planets::calculate(1.0, 2);
+    let calculate = weight_another_planets::calculate_weight(1.0, "h");
     print!("{calculate}");
+
+    weight_another_planets::list_planets_gravity(Some("Tierra")); // Ejemplo: Obtener la gravedad de un planeta específico
+    weight_another_planets::list_planets_gravity(None); // Ejemplo: Mostrar todos los valores de gravedad
 }
--- magic_calculator/src/weight_another_planets.rs
+++ magic_calculator/src/weight_another_planets.rs
@@ -1,8 +1,46 @@
+use std::collections::HashMap;
 
 /*
-Module for calcule your weight on another planet
+Show the differents planets/gravity
 */
-pub fn calculate(weight:f64, planet:i32) -> i32 {
+pub fn list_planets_gravity(planet: Option<&str>) -> Option<f64> {
+    // Map with planets gravity
+    let planets: HashMap<&str, f64> = [
+        ("Marte", 3.71),
+        ("Mercurio", 3.70),
+        ("Urano", 8.69),
+        ("Neptuno", 11.00),
+        ("Saturno", 8.96),
+        ("Jupiter", 23.12),
+        ("Tierra", 9.81),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    if let Some(planet) = planet {
+        if let Some(gravity) = planets.get(planet) {
+            println!("Planet: {} Gravity: {}", planet, gravity);
+            return Some(*gravity);
+
+        } else {
+            println!("Planet {} don't exists.", planet);
+            return None;
+
+        }
+    } else {
+        println!("Show the all gravity for planets:");
+        for (planet, gravity) in &planets {
+            println!("{}: {}", planet, gravity);
+        }
+        return None;
+    }
+}
+
+/*
+Make the calculate
+*/
+pub fn calculate_weight(weight:f64, planet:&str) -> i32 {
     println!("{}, {}", weight, planet);
     return 1;
 }
